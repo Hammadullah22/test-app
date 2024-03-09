2b56d1062dac91b0b7e4c4af73582edf2be7c037
hammadullah22 <hammadullah222@gmail.com>
2024-03-09 16:16:49 +0500

    setuptest.js file

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,15 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+// Remove the default test case for 'Learn React' link
+// import { render } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
